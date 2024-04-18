b27523dff5f4f4250c182cd466a92deb2e504755
ManimaranNCOG <166836528+ManimaranNCOG@users.noreply.github.com>
2024-04-18 23:33:43 +0530

    Update email_registry.sol

--- contracts/registry/email_registry.sol
+++ contracts/registry/email_registry.sol
@@ -1,38 +1,40 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-
 contract HostRegistryContract {
 
+    // Struct to store user data
     struct UserData {
         address contractAddress;
         string userData;
     }
     
+    // Mapping to store user data by username
     mapping(string => UserData) public users;
 
+    // Function to return a simple confirmation string
     function ping() external pure returns (string memory) {
         return "yes";
     }
 
-    // Function to set the address of the contract and string variable based on username
-    function registerChain( string memory username, address _contractAddress,  string memory _userData ) public {
+    // Function to register user data
+    function registerChain(string memory username, address _contractAddress, string memory _userData) public {
         users[username] = UserData(_contractAddress, _userData);
     }
 
-    // Function to get the address of the contract and string variable based on username
+    // Function to get user data by username
     function getChainDetails(string memory username) public view returns (address, string memory) {
         UserData memory userData = users[username];
         return (userData.contractAddress, userData.userData);
     }
 
-    // Function to update the address of the contract and string variable based on username
+    // Function to update user data
     function updateChain(string memory username, address _contractAddress, string memory _userData) public {
         require(users[username].contractAddress != address(0), "User does not exist");
         users[username] = UserData(_contractAddress, _userData);
     }
 
-    // Function to delete user data based on username
+    // Function to delete user data
     function deleteChain(string memory username) public {
         require(users[username].contractAddress != address(0), "User does not exist");
         delete users[username];

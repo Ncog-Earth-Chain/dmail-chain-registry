69283a8aa270de914608bf67ad49c5a183147bc5
ManimaranNCOG <166836528+ManimaranNCOG@users.noreply.github.com>
2024-04-18 23:32:23 +0530

    Update email_pool.sol

--- contracts/pool/email_pool.sol
+++ contracts/pool/email_pool.sol
@@ -9,14 +9,17 @@ contract EmailPoolContract {
     mapping(string => string[]) private emailsByDomain;
     mapping(string => string[]) private emailsForSending;
 
+    // Save emails based on domain
     function saveEmailsBasedOnDomain(string memory domain, string memory email) public {
         emailsByDomain[domain].push(email);
     }
 
+    // Get emails by domain
     function getEmailsByDomain(string memory domain) public view returns (string[] memory) {
         return emailsByDomain[domain];
     }
     
+    // Delete emails based on domain
     function deleteEmailsBasedOnDomain(string memory domain) public {
         string[] storage emails = emailsByDomain[domain];
         for (uint i = 0; i < emails.length; i++) {
@@ -25,14 +28,17 @@ contract EmailPoolContract {
         delete emailsByDomain[domain];
     }
 
+    // Send emails based on domain
     function sendEmailsBasedOnDomain(string memory domain, string memory email) public {
         emailsForSending[domain].push(email);
     }
 
-    function getSendEmailsByDomain(string memory domain) public view  returns (string[] memory) {
+    // Get sent emails by domain
+    function getSendEmailsByDomain(string memory domain) public view returns (string[] memory) {
         return emailsForSending[domain];
     }
 
+    // Delete sent emails based on domain
     function deleteSendEmailsBasedOnDomain(string memory domain) public {
         string[] storage emails = emailsForSending[domain];
         for (uint i = 0; i < emails.length; i++) {
@@ -41,6 +47,7 @@ contract EmailPoolContract {
         delete emailsForSending[domain];
     }
 
+    // Call function in another contract to save email
     function callSendEmailRequest(string calldata username, string calldata sendString , address otherContractAddress) public {
         OtherContractInterface otherContract = OtherContractInterface(otherContractAddress);
         otherContract.saveEmailFromDifferentChain(username, sendString);
